4c1f95caf7671ee7db6107846e5ddce171617a48
Eduardo Felipe de Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-05-19 00:18:33 -0300

    Refactory database model path

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
 import { createConnection } from 'typeorm';
+import * as path from 'path';
 
-createConnection();
+createConnection({
+  type: 'mysql',
+  host: '54.233.223.179',
+  port: 3306,
+  username: 'eduardo',
+  password: 'B@lpha9001',
+  database: 'astpp',
+  logging: false,
+  entities: [
+    path.resolve(__dirname, '..', 'models', '*{.ts,.js}'),
+    // './src/models/*.ts',
+  ],
+});
